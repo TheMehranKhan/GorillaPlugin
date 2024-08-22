745d96e18847e5bfeb40e6750d65257a718a1964
TheMehranKhan <khanmohammadimehran@gmail.com>
2024-08-22 15:35:03 +0330

    initial commit

---
+++ src/main/java/gorilla/com/themehrankhan/gorilla/Com/themehrankhan/gorilla.java
@@ -0,0 +1,17 @@
+package gorilla.com.themehrankhan.gorilla.Com.themehrankhan;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class gorilla extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
